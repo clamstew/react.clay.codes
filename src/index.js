619dede44e04309c98575e8af086048bd3ddaa4e
Clay Stewart <claytonmstewart@gmail.com>
2017-08-06 00:08:34 -0500

    save workspace and push to github

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BlogPost } from './BlogPost';
-import registerServiceWorker from './registerServiceWorker';
+// @TODO - add back when get secure domain
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<BlogPost />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
