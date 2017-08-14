2ce1fa49ca78d46dcee6f7b5dc08b2cf30919348
Clay Stewart <claytonmstewart@gmail.com>
2017-08-13 23:21:39 -0500

    add some initial comments to the init script

---
+++ scripts/init-react-blog.js
@@ -0,0 +1,7 @@
+// adds the default values to the
+// package.json
+//
+// takes user through each confg
+// assume user is in creat react app and add instructions on how to
+// unless they change directories
+// and put custom directions for what to do next
